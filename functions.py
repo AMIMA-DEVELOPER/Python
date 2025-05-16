2c854cc97c72768b95a29fa1e0d496d7f0e7d36b
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-05-16 14:42:35 +0500

    intial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+# def greet(name):
+#     print("hi ! "+ name + " how are you?")
+
+# n= input("enter your name:")
+# greet(n)    
+def y(num):
+    if num>=0:
+        return num
+    else:
+        return -num
+x=y(-36) 
+print(x)
+
+    
