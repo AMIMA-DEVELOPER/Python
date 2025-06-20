3c9d6d680dda6f7cc5b55510f84590146b94dda9
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-06-20 14:43:37 +0500

    initial commit

--- oops.py
+++ oops.py
@@ -10,8 +10,50 @@ class Parrot:
         self.name=name
         self.age=age
 
-Pr=Parrot("mithu",7)
-Tr=Parrot("bithu",8)
+Pr=Parrot("mit",7)
+Tr=Parrot("bit",8)
 print(Pr.name,"is a",Pr.species)    
-print(Tr.name," is ",Tr.age," years old")    
 
+print(Tr.name," is ",Tr.age," years old")    
+print(Pr.name,"efone")
+
+
+#Write a Python class named Rectangle with a length and width and a method that computes the area of a rectangle. Display the dimensions and calculated area of the rectangle as well.
+
+class rectangle:
+    def __init__(self,length,width):   
+        self.length= length
+        self.width= width
+    def area(self):
+        print("AREA OF THE RECTANGLE IS", self.length*self.width)
+         
+length=int(input("Enter the length  "))
+width=int(input("Enter the width    "))
+
+
+
+
+r =rectangle(length,width)
+r.area()        
+
+
+#Print the peremeter and the area of the circle
+
+class circle:
+    def __init__(self,radius):
+       self.radius= radius
+    def peremeter(self):
+    
+        print("The peremeter of the circle is  " ,2*3.142*self.radius)
+
+    def area(self):
+        print("the area of the circle is" ,3.142*self.radius*self.radius)
+
+
+Radius= int(input("The value for the radius  "))
+C= circle(Radius)
+C.peremeter()
+C.area() 
+
+        
+          
