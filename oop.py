ae4d3aeaff1da4171cf1ec7ac8eaf891e55aa3a4
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-06-22 16:44:09 +0500

    initial commit

---
+++ oop.py
@@ -0,0 +1,14 @@
+class triangle:
+    def __init__(self,base,height):
+        self.base= base
+        self.height= height
+    def area (self):
+
+        print("The area of the triangle is " ,0.5*self.base*self.height)
+
+Area= int(input("The value for the area for the triangle:  "))
+
+T= triangle(base)
+ 
+T= triangle(height)
+T.area()
