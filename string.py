f9b0db9b084a3136aaf76233fa6ff098856e4a75
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-07-04 16:24:31 +0500

    initial commit

---
+++ string.py
@@ -0,0 +1,13 @@
+
+
+question = 'How are you today?'
+
+# Using double quotes
+name = "alice"
+response = "I'm doing well, thank you!"
+farewell = "Goodbye, " + name + "!"
+
+print(name)
+print(question)
+print(response)
+print(farewell)
