7b6f45d286a2bd33196ef373556a9df03b694ade
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-07-14 16:15:24 +0500

    initail commit

---
+++ prime.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is prime and print the result
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
