e6e718ff66d6faecfd07877dbc3486b844093adc
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-08-08 13:41:49 +0500

    initial commit

--- coding.py
+++ coding.py
@@ -11,10 +11,22 @@
 #print(f.read())
 #f.close()
 
-f2= open('CODING.TXT','w')
-f2.write('My name is Amima. I study in Grade 7.')
-f2.close()
+#f2= open('CODING.TXT','w')
+#f2.write('My name is Amima. I study in Grade 7.')
+#f2.close()
 
 f= open('CODING.TXT','r')
-print(f.read())
+counter = 0
+
+Content = f.read()
+colist = Content.split("\n")
+
+for i in colist:
+        if i:
+                counter+=1
+print("this is the number of lines:")
+print(counter)
+
+
+
 f.close()
