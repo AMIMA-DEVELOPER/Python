260c91ff29d3735344f55a4eff2895281f6e7ba7
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-08-22 14:38:42 +0500

    initial commit

--- coding1.py
+++ coding1.py
@@ -2,8 +2,17 @@
 #f.write("\nI am 13 years old")
 #f.close()
 
+
+#Write a Python program to remove lines of a file starting with prefix - Coding and store the contents in a new file.
+
+
 f= open('CODING.TXT','r')
-print("Looping through the lines....")
-for line in f:
-    print(line)
+f1= open('Coding12.TXT','w')
+
+for lines in f.readlines():
+    if not (lines.startswith("I am")):
+        print(lines)
+        f1.write(lines)
+
+f1.close()
 f.close()
