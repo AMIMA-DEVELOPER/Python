11b70a17d5cb6659172e705a4c076548c6048a5f
AMIMA-DEVELOPER <amima.umair66@gmail.com>
2025-09-12 14:34:18 +0500

    initial commit

---
+++ file.py
@@ -0,0 +1,23 @@
+# with open('Coding12.TXT','w') as Fl:
+#     Fl.write("My name is Amima.")
+#     Fl.write("\nI am 12 year old.")
+# Fl.close()
+#     #spliting contents
+
+# with open('Coding12.TXT','r') as Fl:
+#     Data = Fl.readlines()
+#     print("Words in this file are.......")
+#     for line in Data:
+ #       word= line.split()
+#        print(word)
+#Fl.close()        
+
+# import os
+# print("check if CODING.TXT exists or not....")
+# if os.path.exists("CODING.TXT"):
+#     os.remove("CODING.TXT")
+# else:
+#     print("The file doesnot exists.")
+
+import os
+os.remove("New file.TXT")
